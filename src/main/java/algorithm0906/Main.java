379dfafefd5aee9f8b64c2180e1f64064d53b02d
quswjdgns399 <quswjdgns3996@gmail.com>
2023-09-06 09:29:12 +0900

    0906 두 수를 뒤집어 더하고 다시 뒤집기

---
+++ src/main/java/algorithm0906/Main.java
@@ -0,0 +1,50 @@
+package algorithm0906;
+
+public class Main {
+
+    private static double correctCount = 0;
+
+    private static final String[][] TESTCASES = new String[][] {
+            {"123", "123", "246"},
+            {"1000", "1", "2"},
+            {"456", "789", "1461"},
+            {"5", "5", "1"},
+            {"11112", "54321", "65433"},
+            {"3829", "1300", "4139"}
+    };
+
+    public static void main(String[] args) {
+        for (int i = 0; i < TESTCASES.length; i++) {
+            System.out.println("Testcase " + i + " = " + test(TESTCASES[i][0], TESTCASES[i][1], TESTCASES[i][2]));
+        }
+
+        System.out.println("정답률 = " + (int) (correctCount / TESTCASES.length * 100) + "%");
+    }
+
+    private static boolean test(String input, String input2, String answer) {
+        int first = Integer.parseInt(input);
+        int second = Integer.parseInt(input2);
+
+        boolean res = String.valueOf(solution(first, second)).equals(answer);
+        if (res) {
+            correctCount++;
+        }
+        return res;
+    }
+
+    // solution
+    public static int solution(int a, int b) {
+        return reverse(reverse(a) + reverse(b));
+    }
+
+    static int reverse(int n) {
+        char[] arr = String.valueOf(n).toCharArray();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = arr.length - 1; i > -1; i--) {
+            sb.append(arr[i]);
+
+        }
+        return Integer.reverse(Integer.parseInt(sb.toString()));
+    }
+}
