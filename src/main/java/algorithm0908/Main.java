d407036e78476a20d72043a83e8c53eab77a63c8
quswjdgns399 <quswjdgns3996@gmail.com>
2023-09-08 09:36:45 +0900

    0908 소수판별 / 제급근과 에라토스테네스 알고리즘 활용

--- src/main/java/algorithm0908/Main.java
+++ src/main/java/algorithm0908/Main.java
@@ -1,5 +1,7 @@
 package algorithm0908;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static int solution(int start, int end) {
@@ -25,11 +27,18 @@ public class Main {
         if (num == 2) {
             return true;
         }
-        for (int i = 2; i <= num - 1; i++) {
+//        for (int i = 2; i <= num - 1; i++) {
+//            if (num % i == 0) {
+//                return false;
+//            }
+//        }
+
+        for (int i = 2; i * i <= num; i++) {
             if (num % i == 0) {
                 return false;
             }
         }
+
 
         return true;
     }
@@ -41,6 +50,30 @@ public class Main {
         }
 
         System.out.printf("정답률 = %.3f%%", (correct / TEST_CASES.length * 100));
+
+
+        //  에라토스테네스 알고리즘
+        int arr[] = new int[101];
+        boolean[] prime = new boolean[101];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+        }
+
+        prime[0] = true;
+        prime[1] = true;
+
+        for (int i = 2; i < Math.sqrt(arr.length); i++) {
+            if (!isPrime(i)) {
+                for (int j = i; j < arr.length; j += i) {
+                    prime[j] = true;
+                    //false == 소수
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(prime));
+
     }
 
 
