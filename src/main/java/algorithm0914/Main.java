8a41ad89b7be9812550fb532e42416c81f0c96da
quswjdgns399 <quswjdgns3996@gmail.com>
2023-09-14 10:56:53 +0900

    0914  소수를 분수로 변경 / gcd사용

---
+++ src/main/java/algorithm0914/Main.java
@@ -0,0 +1,90 @@
+package algorithm0914;
+
+import java.math.BigDecimal;
+
+public class Main {
+
+    public static class Fraction {
+        private int numerator;  // 분자
+        private int denominator;    // 분모
+
+        public Fraction(int numerator, int denominator) {
+            this.numerator = numerator;
+            this.denominator = denominator;
+
+            normalize();
+        }
+
+        public int getNumerator() {
+            return numerator;
+        }
+
+        public int getDenominator() {
+            return denominator;
+        }
+
+
+        public void normalize() {
+            int gcd = calculateGCD(numerator, denominator);
+            this.numerator /= gcd;
+            this.denominator /= gcd;
+        }
+
+
+        private static int calculateGCD(int a, int b) {
+            if (b == 0) {
+                return a;
+            }
+            return calculateGCD(b, a % b);
+        }
+
+
+        /**
+         * 소수점 값에 대해 자리 수 계산 하는 메서드.    <br/>
+         * ex)  <br/>
+         * 0.01 -> 100을 곱하기 위해 2를 return    <br/>
+         * 0.1 -> 10을 곱하기 위해 1을 return
+         *
+         * @param num 소수 값
+         * @return 소수점 이하 자리 수 값
+         */
+        public static int getDecimalNumber(BigDecimal num) {
+
+            String str = num.toPlainString();
+
+            int count = 0;
+            for (int i = 0; i < str.length(); i++) {
+
+                if (str.charAt(i) == '.') {
+                    count = str.length() - i;
+                    break;
+                }
+            }
+            return count;
+        }
+
+        public static String solution(String input) {
+            BigDecimal number = new BigDecimal(input);
+
+            int decimalNumber = getDecimalNumber(number);
+
+            int numerator = (int) (Double.parseDouble(input) * Math.pow(10, decimalNumber));
+            double denominator = Math.pow(10, decimalNumber);
+
+
+            return new Fraction(numerator, (int) denominator).toString();
+        }
+
+        @Override
+        public String toString() {
+            return this.numerator + "/" + this.denominator;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        String result = Fraction.solution("23456.98765");
+        System.out.println(result.toString());
+    }
+
+}
